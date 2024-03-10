9b951d83cd2ed855d8f7084c84444ba7eeed7278
Farid Mammadbayov <mm.farid@post.cz>
2024-03-10 16:25:44 +0100

    feature/source google docs added

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,14 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
+    {
+      resolve: "gatsby-source-google-docs",
+      options: {
+        // https://drive.google.com/drive/folders/FOLDER_ID
+        folder: "1kQeIoZ2Hf-fE6AEJnyiMbh72VaT5PI-I",
+        createPages: true,
+      },
+    },
     "gatsby-source-google-docs",
     "gatsby-transformer-remark",
     "gatsby-source-google-docs",
